edcd993c2235428e7509ed670cd6d7c42f5233be
PhanThiKimNgan-24022699 <24022699@vnu.edu.vn>
2025-11-25 14:39:45 +0700

    Update

---
+++ Kruskal.cpp
@@ -0,0 +1,28 @@
+struct Edge {
+    int u, v, w;
+};
+
+struct DSU {
+    vector<int> parent, sz;
+
+    DSU(int n) {
+        parent.resize(n+1);
+        sz.resize(n+1, 1);
+        for(int i=1; i<=n; i++) parent[i] = i;
+    }
+
+    int find(int x) {
+        if(parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    bool unite(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if(a == b) return false;
+        if(sz[a] < sz[b]) swap(a, b);
+        parent[b] = a;
+        sz[a] += sz[b];
+        return true;
+    }
+};
