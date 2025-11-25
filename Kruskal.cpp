675076fd71e9a43b1d74e487e7cdfc6ae3424f8b
PhanThiKimNgan-24022699 <24022699@vnu.edu.vn>
2025-11-25 14:43:50 +0700

    Add

--- Kruskal.cpp
+++ Kruskal.cpp
@@ -8,7 +8,7 @@ struct DSU {
     DSU(int n) {
         parent.resize(n+1);
         sz.resize(n+1, 1);
-        for(int i=1; i<=n; i++) parent[i] = i;
+        for(int i = 1; i <= n; i++) parent[i] = i;
     }
 
     int find(int x) {
@@ -26,3 +26,21 @@ struct DSU {
         return true;
     }
 };
+
+int kruskalMST(int n, vector<Edge>& edges) {
+    sort(edges.begin(), edges.end(),
+         [](const Edge& a, const Edge& b) {
+             return a.w < b.w;
+         });
+
+    DSU dsu(n);
+    int mst_weight = 0;
+
+    for(const auto& e : edges) {
+        if(dsu.unite(e.u, e.v)) {
+            mst_weight += e.w;
+        }
+    }
+
+    return mst_weight;
+}
